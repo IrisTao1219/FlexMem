7756059a948e914861f3a585be41ca94f34e366c
Iris Tao <1010939908@qq.com>
2026-07-30 10:37:26 +0800

    feat: 实验结果

--- FlexMem/scripts/video/lvbench/lvbench_eval_native.sh
+++ FlexMem/scripts/video/lvbench/lvbench_eval_native.sh
@@ -16,7 +16,7 @@ CKPT="/home/taosha/models/Hf_model/LLaVA-Video-7B-Qwen2" #Your Model Path
 DATA_ROOT="/home/taosha/datasets/Datasets/LongVideoBench" #Your LongVideoBench Root
 VIDEO_DIR=${DATA_ROOT}/videos
 # GT_FILE=${DATA_ROOT}/lvb_val.json
-GT_FILE="../bench/lvb_val_quick.json"
+GT_FILE="../bench/lvb_val_quick_15.json"
 CONV_MODE=qwen_1_5
 POOL_STRIDE=2
 OVERWRITE=True
@@ -45,7 +45,7 @@ echo $SAVE_DIR
 
 CHUNKS=1
 # Assuming GPULIST is a bash array containing your GPUs
-GPULIST=(0)
+GPULIST=(3)
 
 # Get the number of GPUs
 NUM_GPUS=${#GPULIST[@]}
